f5944a3bf4dd7d4b8c8a4940d69349a83b39d8ff
Ethan Black <ethanrossblack@gmail.com>
2023-04-13 12:04:36 -0600

    Complete sections 1 and 2

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -8,19 +8,26 @@
 # The return value is "hello world"
 "Hello World".downcase
 
-
+# This code calls the include? method to check if the string object "Hello World" contains the argument "Hello" and returns a boolean value
+# The return value is true
 "Hello World".include?("Hello")
 
-
+# This code calls the endwith? method to check if the string object "Hello World" ends with the argument "Hello" and returns a boolean value
+# The return value is false
 "Hello World".end_with?("Hello")
 
-
+# This code calls the endwith? method to check if the string object "Hello World" ends with the argument "rld" and returns a boolean value
+# The return value is true
 "Hello World".end_with?("rld")
 
-
+# This code calls the even? method to check if the integer 12 is an even number and returns a boolean value
+# The even method receives no arguments 
+# The return value is true
 12.even?
 
-
+# This code calls the next method on the integer 18
+# No arguments are passed; next adds one to the provided integer
+# The return value is 19
 18.next
 
 
@@ -39,6 +46,21 @@
 first_name = "Jeff"
 puts first_name.start_with?("J")
 
+
+# The center method is called on the full_name variable, which stores the string object "Ethan Black".
+# The center method returns the full_name variable centered in a string with the length of the first argument passed in and padded by a repeated string of the second argument passed in.
+# In this exampled, the return value is "~~~~~~~~~~~~~~~Ethan Black~~~~~~~~~~~~~~~", a string 41 characters long that centers the string "Ethan Black" padded by repetitions of the string "~"
+# The puts command prints the return value of the center method (~~~~~~~~~~~~~~~Ethan Black~~~~~~~~~~~~~~~) to the console.
+full_name = "Ethan Black" 
+puts full_name.center(41, "~")
+
+# The index method is called on the school variable, which stores the string object "Turing"
+# The index method returns the integer index of the first occurence of the argument string passed in.
+# In this example, the first occurence of the argument string "u" appears at the 1 index of the string object "Turing".
+# The puts command prints the return value of the index method (1) to the console.
+
+school = "Turing"
+puts school.index("u")
 
 
 # SECTION 3: Calling methods on variables assigned to integers.
